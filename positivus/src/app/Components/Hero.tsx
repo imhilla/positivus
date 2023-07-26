ae345d3e736d5247d0f4b71253f1417126c9f437
imhilla <hillaryodhiambo282@gmail.com>
2023-07-26 16:17:41 +0300

    update styles

--- positivus/src/app/Components/Hero.tsx
+++ positivus/src/app/Components/Hero.tsx
@@ -1,12 +1,56 @@
 import useIsMobile from '@/Hooks/resizeHooks'
 import React from 'react'
+import Image from 'next/image'
+import Illustration from '../../../public/Illustration.png'
 
 export default function Hero() {
   const isMobile = useIsMobile()
 
   return (
-    <div style={{ paddingTop: 40 }}>
-      <h1 style={{ fontSize: isMobile ? 30 : 60 }}>Navigating the digital landscape for success</h1>
+    <div style={{ paddingTop: 40 }} className='lg:flex flex-row'>
+      <div>
+        <h1 style={{ fontSize: isMobile ? 30 : 60 }}>Navigating the digital landscape for success</h1>
+        <p className='hidden lg:block mt-4'>
+          Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+        </p>
+        <button
+          className='hidden lg:block mt-4'
+          style={{
+            width: 264,
+            height: 68,
+            backgroundColor: '#191A23',
+            borderRadius: 14,
+            fontSize: 20
+          }}
+        >
+          Book a consultation
+        </button>
+      </div>
+      <div className='pt-4 lg:pt-0 lg:h-3/4 w-full h-full lg:flex items-end justify-center'>
+        <Image
+          className='w-full lg:w-3/4 lg:h-3/4'
+          src={Illustration}
+          alt="logo"
+          unoptimized
+        />
+      </div>
+      <div>
+        <p className='lg:hidden mt-4'>
+          Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+        </p>
+        <button
+          className='lg:hidden mt-4 text-white'
+          style={{
+            width: 264,
+            height: 68,
+            backgroundColor: '#191A23',
+            borderRadius: 14,
+            fontSize: 20,
+          }}
+        >
+          Book a consultation
+        </button>
+      </div>
     </div>
   )
 }

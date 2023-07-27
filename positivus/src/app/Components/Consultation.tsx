89fdbdec8a8d56b569b465fe5ac2dade4a47be8e
imhilla <hillaryodhiambo282@gmail.com>
2023-07-27 10:36:35 +0300

    updates

---
+++ positivus/src/app/Components/Consultation.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//@ts-ignore
+export default function Consultation(props) {
+  return (
+    <div>Consultation</div>
+  )
+}
--- positivus/src/app/Components/WorkingProcess.tsx
+++ positivus/src/app/Components/WorkingProcess.tsx
@@ -1,5 +1,6 @@
 import useIsMobile from '@/Hooks/resizeHooks'
 import React from 'react'
+import Consultation from './Consultation'
 
 export default function WorkingProcess() {
   const isMobile = useIsMobile()
@@ -51,6 +52,14 @@ export default function WorkingProcess() {
         <p style={{ fontSize: isMobile ? 16 : 18, marginTop: isMobile ? 30 : 0 }} className='text-center lg:text-start lg:ml-4 lg:w-1/2'>
           Step-by-Step Guide to Achieving Your Business Goals
         </p>
+      </div>
+      <div>
+        <Consultation textNumber={"01"} title={"Consultation"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
+        <Consultation textNumber={"02"} title={"Research and Strategy Development"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
+        <Consultation textNumber={"03"} title={"Implementation"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
+        <Consultation textNumber={"04"} title={"Monitoring and Optimization"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
+        <Consultation textNumber={"05"} title={"Reporting and Communication"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
+        <Consultation textNumber={"06"} title={"Continual Improvement"} paraText={"During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements."} />
       </div>
     </div>
   )
